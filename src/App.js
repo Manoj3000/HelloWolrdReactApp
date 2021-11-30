80cd5d5214cb37aa924bc2730afb76ebdf5873b1
manoj gaikwad <gaikwadm853@gmail.com>
2021-11-30 23:47:24 +0530

    Added regex on name

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState("");
+  const [nameError, setNameError] = useState("");
 
   const openSite = () => {
     window.open("https://www.bridgelabz.com/", "_blank")
+  }
+
+  const inputHandler = (e) => {
+    const nameRegex = new RegExp("[A-z]{1}[a-zA-Z\\s]{2,}$");
+
+    console.log(e.target.value);
+    setName(e.target.value)
+    
+    if (nameRegex.test(e.target.value)) {
+      setNameError("")
+    } else {
+      setNameError("Invalid Name!")
+    }
+    
   }
 
   return (
@@ -14,7 +29,8 @@ function App() {
       <h1>Hello {name == "" ? "World" : name} From BridgeLabz</h1>
       <img src={logo} onClick={openSite} alt="BridgeLabz Logo" className="img_btn" />
       <div>
-        <input className="input" type="text" value={name} onChange={e => setName(e.target.value)} />
+        <input className="input" type="text" value={name} onChange={inputHandler} /><br />
+        <span>{nameError}</span>
       </div>
     </div>
   );
