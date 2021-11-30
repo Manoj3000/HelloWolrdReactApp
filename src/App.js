50974850a3fd1a6168774117af6869744e8888f9
manoj gaikwad <gaikwadm853@gmail.com>
2021-11-30 22:47:31 +0530

    Added: open BridgeLabz site on click on the logo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 
 function App() {
   const [message, setMessage] = useState("Hello World From BridgeLabz");
+
+  const openSite = () =>{
+    window.open("https://www.bridgelabz.com/", "_blank")
+  }
+
   return (
     <div className="App">
       <h1>{message}</h1>
-      <img src={logo} alt="BridgeLabz Logo" />
+      <img src={logo} onClick={openSite} alt="BridgeLabz Logo" className="img_btn" />
     </div>
   );
 }
