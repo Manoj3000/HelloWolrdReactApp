58c499807286a8b37e5ecface0ddcd99c93aa95e
manoj gaikwad <gaikwadm853@gmail.com>
2021-11-30 21:41:31 +0530

    Added BridgeLabz Logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';  
+import React, { useState } from 'react';
+import logo from "./assets/logo.png";
 import './App.css';
 
 function App() {
   const [message, setMessage] = useState("Hello World From BridgeLabz");
   return (
     <div className="App">
-     <h1>{message}</h1>
+      <h1>{message}</h1>
+      <img src={logo} alt="BridgeLabz Logo" />
     </div>
   );
 }
