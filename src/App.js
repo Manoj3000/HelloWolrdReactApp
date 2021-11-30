5d6b7d2c4a94f0e61629244c0751a7be5039ff9a
manoj gaikwad <gaikwadm853@gmail.com>
2021-12-01 00:40:55 +0530

    Added Form UI

--- src/App.js
+++ src/App.js
@@ -15,22 +15,41 @@ function App() {
 
     console.log(e.target.value);
     setName(e.target.value)
-    
+
     if (nameRegex.test(e.target.value)) {
       setNameError("")
     } else {
       setNameError("Invalid Name!")
     }
-    
+
   }
 
   return (
     <div className="App">
-      <h1>Hello {name == "" ? "World" : name} From BridgeLabz</h1>
-      <img src={logo} onClick={openSite} alt="BridgeLabz Logo" className="img_btn" />
-      <div>
-        <input className="input" type="text" value={name} onChange={inputHandler} /><br />
-        <span>{nameError}</span>
+      <div className="container">
+        <div className="center">
+          <h1 className="heading">Hello {name == "" ? "World" : name} From BridgeLabz</h1>
+          <img src={logo} onClick={openSite} alt="BridgeLabz Logo" className="img_btn" />
+          <div>
+            <input className="input" type="text" value={name} onChange={inputHandler} /><br />
+            <span className="errorMessage">{nameError}</span>
+          </div>
+        </div>
+        <p>At Bridge Labz, we're a techie community of</p>
+        <ul className="list">
+          <li>technologists</li>
+          <li>thinkers</li>
+          <li>builders</li>
+        </ul>
+        <p>Working together to keep the Tech Employability of Engineers alive and accessible, so
+          Tech Companies worldwide can get contributors and creators for Technology Solutions.
+          We believe this act of human collaboration across an employability platform is essential
+          to individual growth and our collective future.
+        </p>
+        <br />
+        <p>To Know about us, visit <a target="_blank" href="https://www.bridgelabz.com/">BridgeLabz</a> to learn even more about our mission
+          <br /> i.e.<span className="bolder">Employability to all</span>
+        </p>
       </div>
     </div>
   );
