9067a45018c6ef57a94758af50e6c2c0f4ca049c
manoj gaikwad <gaikwadm853@gmail.com>
2021-11-30 23:07:44 +0530

    Added input tag to take  name

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import logo from "./assets/logo.png";
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState("Hello World From BridgeLabz");
+  const [name, setName] = useState("");
 
-  const openSite = () =>{
+  const openSite = () => {
     window.open("https://www.bridgelabz.com/", "_blank")
   }
 
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <h1>Hello {name == "" ? "World" : name} From BridgeLabz</h1>
       <img src={logo} onClick={openSite} alt="BridgeLabz Logo" className="img_btn" />
+      <div>
+        <input className="input" type="text" value={name} onChange={e => setName(e.target.value)} />
+      </div>
     </div>
   );
 }
